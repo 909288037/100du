08d0910f978841cfb45e6ecdbcce60111685cc53
liushuai <909288037@qq.com>
2017-07-25 00:45:45 +0800

    更新至今日推荐，轮播图板块已完成

--- js/index.js
+++ js/index.js
@@ -49,6 +49,73 @@ $(window).ready(function () {
                 // console.log(this.innerHTML);
             })
         });
+//---------------轮播图-----------------
+    var index = 0;
+    var timer;
+    var liArr = $(".slider-fade>li");
 
-})
+//触摸显示箭头导航
+    $(".jd-slider").on("mouseover",function () {
+        $(".jd-slider>div").show();
+        clearInterval(timer);
+    });
+    $(".jd-slider").on("mouseout",function () {
+        $(".jd-slider>div").hide();
+        timer = setInterval(autoSilder,3000);
+    });
+
+    //点击左右导航切换图片
+    $(".prev").on("click",function () {
+        index--;
+        if( index < 0 ){
+            index=liArr.length-1;
+        }
+        $(".slider-fade-index li").eq(index).addClass("active").siblings().removeClass("active");
+        $(".slider-fade li").hide().eq(index).fadeIn(1000).siblings().fadeOut(1000);
+    });
+    $(" .next").on("click",function () {
+       autoSilder();
+    });
+
+    //点击原点索引换图
+    $(".slider-fade-index li").on("mouseenter",function () {
+        index = $(this).index();
+        $(".slider-fade-index li").eq(index).addClass("active").siblings().removeClass("active");
+        $(".slider-fade li").eq(index).stop().fadeIn(300).siblings().stop().fadeOut(300);
+    })
+
+
+    //使用定时器之前先关闭
+    clearInterval(timer);
+    //自动轮播
+    timer = setInterval(autoSilder,1500);
+        //切换图片函数
+        function autoSilder() {
+            index++;
+            if(index == liArr.length){
+                index = 0;
+            }
+            $(".slider-fade-index li").eq(index).addClass("active").siblings().removeClass("active");
+            $(".slider-fade>li").eq(index).fadeIn(300).siblings().fadeOut(300);
+        }
+//---------------------------轮播图end----------------------
+//-----------------------轮播图底下轮播图-------------------
+var ulW = $(".today-r ul").width();
+var liIndex = 0;
+var timers;
+    clearInterval(timers);
+    autoPlay();
+    function autoPlay() {
+        timers = setInterval(function () {
+            liIndex++;
+            if(liIndex>2){
+                liIndex=0;
+                $(".today-r").css({marginLeft:0});
+            }
+            $(".today-r").animate({marginLeft:-ulW*liIndex})
+        },3000)
+    }
+
+
+});
 
